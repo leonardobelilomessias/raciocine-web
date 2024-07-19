1246c59b200a5b3bca66347d7fabe163618cb91d
leonardobelilomessias <leonardobelilo@outlook.com>
2024-07-19 13:07:01 -0300

    add tag maneger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 // import { Rubik, Rajdhani } from 'next/font/google'
+import Script from "next/script"
 import "../styles/index.scss"
 
 
@@ -25,12 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={isDev}>
       <head>
+        <Script dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MSHNKBQH');`}}/>
         <meta name="facebook-domain-verification" content="sofmw23mr36z94i6ffw62rlw3xc3x8" />
         <meta name="description" content="Raciocine - Soluçoes Inteligentes" />
         <link rel="icon" href="/favicon.webp" sizes="any" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rajdhani:400,500,600,700|Poppins:400,500,600,700|Rubik:400,500,600,700|family=Inter:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700&display=swap" />
       </head>
       <body suppressHydrationWarning={true} className="sc5">
+        <Script dangerouslySetInnerHTML={{__html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSHNKBQH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}/>
         {/* <body suppressHydrationWarning={true} className={`sc5 ${body.variable} ${heading.variable}`}> */}
         {children}
       </body>
