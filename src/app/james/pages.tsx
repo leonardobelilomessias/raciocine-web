36b8d6ac0debf56da5a0ad62bfffa41bd9449625
Leonardo Belilo <leonardobelilo@outlook.com>
2024-12-23 08:34:00 -0300

    start create page qrcode

--- src/app/james/pages.tsx
+++
@@ -1,5 +0,0 @@
-
-export default function index(){
-    return(<>
-    <div><h1>texte</h1></div></>)
-}
---
+++ src/app/qrcode/page.tsx
@@ -0,0 +1,29 @@
+
+export default function index(){
+    return(
+    <>
+    <div className="container">
+        <h1 className="display-4 text-center fw-bold"><strong>Alcance mais clientes e aumente seus lucros</strong></h1>
+    <p className="text-center">Somos Especialisas em desenvolvimento de tecnologia para aceleração de Negocios.</p>
+
+    <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/L8eZkeWvAtM?si=mf9-hpNg7UEJpcjY&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <div  style={{backgroundColor:"gray", display:"flex"}}>
+        <div >
+            <div style={{display:"flex"}}>
+                <p >0</p>
+                <p >6</p>
+            </div>
+            <div>Vagas Disponiveis</div>
+        </div>
+        <div style={{display:"flex"}} >
+            <p >
+
+        Se inscreva e garanta uma analize de viabilidade da aceleração do seu negocio 
+            </p>
+        </div>
+    </div>
+    </div>
+    </>)
+}
